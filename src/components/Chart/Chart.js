b82cee453eda5e3b3e26a3db53da945b7a0365b4
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-10-12 17:10:18 +0600

    chart added

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import Header from '../Header/Header';
 
 const Chart = () => {
+    const data = [
+        {
+            name: 'React',
+            total: 8,
+           
+        },
+        {
+            name: 'Javascript',
+            total: 9,
+           
+        },
+        {
+            name: 'CSS',
+            total: 8,
+           
+        },
+        {
+            name: 'Git',
+            total: 11,
+            
+        },
+        
+    ];
+
+
     return (
-        <div>
+        <div className='bg-slate-200 mt-16 ml-32 mr-32 p-10'>
          
-           <h3>iopopsk</h3>
+           <LineChart width={800} height={400} data={data}>
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip></Tooltip>
+           </LineChart>
+            
         </div>
     );
 };
